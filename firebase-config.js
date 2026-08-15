1e6527e1a8e0a3d3c15be34fd4ba4a45ef6446b6
Lanka Olaka <olakalanka12@gmail.com>
2026-08-16 00:18:53 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// ============================================================
+// Firebase config for Logic Laundry Services
+// ============================================================
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDxph2ufVkuZifi40y4g83d2eosl_oNLgg",
+  authDomain: "logic-laundry-services.firebaseapp.com",
+  projectId: "logic-laundry-services",
+  storageBucket: "logic-laundry-services.firebasestorage.app",
+  messagingSenderId: "769994721306",
+  appId: "1:769994721306:web:11cd46a3f2e6b8755629a3"
+};
